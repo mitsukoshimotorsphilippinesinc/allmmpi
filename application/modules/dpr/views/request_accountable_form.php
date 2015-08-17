6ea8df648e2600bad0eb58849345fe9391a0f8f0
edwardcute <joward12@yahoo.com.ph>
2015-08-17 17:25:16 +0800

    as of 08172015

---
+++ application/modules/dpr/views/request_accountable_form.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+
+<title>Accountable Forms Request</title>
+
+<?PHP
+?>
+
+</head>
+
+<body>
+
+	<Label>Select Status</Label>
+	<Select>
+	<option>All<option>
+	<option>Pending<option>
+	<option>Received<option>
+</body>
+
+</html>
+
+ <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+ 
+<SCRIPT TYPE = "text/javascript">
+
+</SCRIPT>
