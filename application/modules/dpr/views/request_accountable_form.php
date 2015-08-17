d778c1b9fb51bcf58d486c095cfd76a573c78176
edwardcute <joward12@yahoo.com.ph>
2015-08-17 17:45:36 +0800

    08172015

--- application/modules/dpr/views/request_accountable_form.php
+++ application/modules/dpr/views/request_accountable_form.php
@@ -15,11 +15,30 @@
 	<option>All<option>
 	<option>Pending<option>
 	<option>Received<option>
+
+	<table class='table table-striped table-bordered'>
+	<thead>
+		<tr>			
+			<th style='width:100px;'>Date Requested</th>
+			<th style='width:80px;'>Reference No.</th>
+			<th style=''>Branch</th>
+			<th style=''>TIN</th>
+			<th style='width:50px;'>Type Of Form</th>
+			<th style='width:100px;'>QTY</th>		
+			<th style='width:118px;'>Printing Press</th>
+			<th style='width:118px;'>Status</th>
+			<th style='width:118px;'>Action</th>
+		</tr>
+	</thead>
+	</table>
+
+	<Button>Create New Request</button>
+		
 </body>
 
 </html>
 
- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
  
 <SCRIPT TYPE = "text/javascript">
 
