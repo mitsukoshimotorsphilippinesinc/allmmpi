3bc0531456c666d50ecef84bad0f1d14875dd4d7
edwardcute <joward12@yahoo.com.ph>
2015-08-29 11:25:15 +0800

    as of 08292015

---
+++ application/modules/dpr/controllers/branch.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Branch extends Admin_Controller {
+
+
+	public function index()
+	{
+		$this->template->view('/branch/dashboard');
+	}
+
+}
